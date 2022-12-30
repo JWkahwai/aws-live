b23538b1fee83ae87e92b903896733ca913685a7
JWkahwai <103020993+JWkahwai@users.noreply.github.com>
2022-12-31 00:38:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.cpgv8rvwzl4n.us-east-1.rds.amazonaws.com"
+customhost = "staff.cpgv8rvwzl4n.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "kahwai-employee"
+custombucket = "kongkahwai-staff"
 customregion = "us-east-1"
